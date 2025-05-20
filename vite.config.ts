e493be4889d9fa0154c96d34f17e01b17ecd0d32
alysonhower <contato@alysonhower.dev>
2025-05-19 22:02:03 -0300

    feat!: upgrade core stack
    - Replace SvelteKit with Svelte for a leaner frontend architecture.
    - Enable runes mode by default.
    - Upgrade to Tailwind CSS 4.0 and remove legacy Tailwind configuration files.
    - Modularize Tauri commands and enhance error handling mechanisms.
    - Refactor "HelloWorld" example into a separate component, including new functionalities.
    - Implemente new Prettier and ESLint configurations.
    - Update project dependencies to their latest versions.
    - Ensure `package-lock.json` is now tracked by version control.
    - Switch Node.js version management from Bun to NVM.
    - Update CI/CD workflows and Renovate bot configuration.
    - Convert repository in template resolve #1

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	clearScreen: false,
+	plugins: [svelte({ compilerOptions: { runes: true } }), tailwindcss()],
+	resolve: {
+		alias: {
+			$lib: path.resolve('./src/lib')
+		}
+	},
 	server: {
 		port: 1420,
 		strictPort: true,
 		host: host || false,
-		hmr: host
-			? {
-				protocol: "ws",
-				host,
-				port: 1421,
-			}
-			: undefined,
+		hmr: host ? { protocol: 'ws', host, port: 1421 } : undefined,
 		watch: {
-			ignored: ["**/src-tauri/**"],
-		},
+			ignored: ['**/src-tauri/**']
+		}
 	},
-	resolve: {
-		alias: {
-			$lib: path.resolve("./src/lib"),
-		},
-	},
+	envPrefix: ['VITE_', 'TAURI_ENV_*'],
+	build: {
+		target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+		minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+		sourcemap: !!process.env.TAURI_ENV_DEBUG
+	}
 });
